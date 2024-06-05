6be532de89ace6da88a534955ece7d90bb866686
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2024-06-05 15:59:59 +0530

    responding to request

--- src/Server.cs
+++ src/Server.cs
@@ -4,3 +4,6 @@ using System.Net.Sockets;
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 server.AcceptSocket(); // wait for client
+const String version = "HTTP/1.1";
+int statusCode = 200;
+Console.Write($"{version} {statusCode} OK\r\n\r\n");
